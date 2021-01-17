247270aa11dae07b1c4385f52dbb49012fc3c5e2
Colour93 <colour_93@furry.top>
2021-01-17 23:39:35 +0800

    temp_update

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
 // 参数
 
-// 监听端口
-const listenPort = 3000
+// 环境
+const env = 'dev'
 
+// 监听端口
+const listenPort = 2998
 
 // API地址
 // const APIURL = 'api.deadlist.fun:29999'
@@ -13,6 +15,7 @@ const i_version = '1.0.10'
 
 module.exports = {
     listenPort,
+    env,
     APIURL,
     i_version
 }
